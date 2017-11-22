5ea0d09735b10a66e6a40c7308fac59d42202c12
Jo <jo.learnstuff@gmail.com>
2017-11-23 00:01:03 +0800

    練習 Angular 內嵌繫結與屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title:string = "The Will Will Web";
   constructor() { }
 
   ngOnInit() {
